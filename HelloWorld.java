05d6e4294d4cdecc21be18d6192fed205dc1a34a
vishnupemmasani437 <68598168+vishnupemmasani437@users.noreply.github.com>
2020-10-28 14:35:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! ******* Welocme to Jenkins"); 
+        System.out.println("Hello, World! ******* Welocme to Jenkins SCM Polling"); 
     }
 }
